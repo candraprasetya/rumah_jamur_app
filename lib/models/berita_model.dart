6f6ae2fbdd3923e916f52b436a9fe5801cb8264e
Candra Prasetya <candraramadhanp@gmail.com>
2020-12-20 07:25:08 +0800

    membuat model berita

---
+++ lib/models/berita_model.dart
@@ -0,0 +1,13 @@
+part of 'models.dart';
+
+class BeritaModel {
+  final String judul;
+  final String imageUrl;
+  final String content;
+  final String uid;
+  final String nama;
+  final DateTime date;
+
+  BeritaModel(
+      this.judul, this.imageUrl, this.content, this.uid, this.nama, this.date);
+}
